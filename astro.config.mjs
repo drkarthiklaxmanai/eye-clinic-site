b74bf9f25612e9d3c584367bacc4e45e858df018
drkarthiklaxmanai <drkarthiklaxmanai@gmail.com>
2026-05-03 11:09:50 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://crispreyecare.com',
+  site: 'https://crispreyecare.com', // <-- This is the critical line causing the crash!
   integrations: [
     tailwind(),
     sitemap()
