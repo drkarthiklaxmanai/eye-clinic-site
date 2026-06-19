1968e0f9f1af5fba0a9d097b531544ab292715f9
drkarthiklaxmanai <drkarthiklaxmanai@gmail.com>
2026-06-20 00:46:25 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // THIS is the line that fixes the crash. It must be here!
   site: 'https://crispreyecare.com',
   prefetch: true,
   integrations: [
-    tailwind(),
-    sitemap(),
-  ],
+    tailwind()
+  ]
 });
